9e894a6d0de197181a6d74e6d28d29e21bb6128b
Gamze <bilgegamze227@outlook.com>
2024-12-19 03:36:40 +0300

    Kontrol Yapıları

---
+++ Kontrol Yapıları/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Write("Lütfen bir sayı giriniz: ");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < 10)
+{
+    Console.WriteLine("Girilen sayı 10 dan küçüktür.");
+   
+}
+
+else if (number > 10)
+{
+    Console.WriteLine("Girilen sayı 10 dan büyüktür.");
+   
+}
+
+else
+{
+    Console.WriteLine("Girilen sayı 10 a eşittir.");
+  
+
+}
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir.");
+}
